7e6aff2446caafd2c9fd0d6f90a87a7b7ce7e501
mrozinskipatryk <patryk.mrozinski@monterail.com>
2023-03-14 20:55:57 +0400

    add App js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import "./App.scss";
+
+function App() {
+  return (
+    <BrowserRouter>
+    </BrowserRouter>
+  );
+}
+
+export default App;
